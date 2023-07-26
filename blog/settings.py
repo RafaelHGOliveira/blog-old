271e4e5888203965efb650f67fb9d3ebe328ba0e
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-26 17:51:51 -0300

    trying to deplou on Railway

--- blog/settings.py
+++ blog/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,9 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (Path(BASE_DIR, 'templates/static'),)
+STATICFILES_DIRS = [
+    Path(BASE_DIR, "static")
+]
 
-MEDIA_ROOT = Path(BASE_DIR, 'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = 'media/'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
